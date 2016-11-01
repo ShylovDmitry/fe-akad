f897ff3bd271aac6e335ddaa89d897fb8951f57d
MaxShylov <shma9013@gmail.com>
2016-11-01 21:28:02 +0200

    Сделал что placeholder поднимался вверх

---
+++ Max/js/placeholderUp.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Макс on 01.11.2016.
+ */
+var a = document.getElementById('input'),
+    p = document.getElementById('placeholder');
+
+g = function(name) {
+    return document.getElementById(name);
+};
+
+function up(a,p) {
+
+    a.onfocus = function () {
+        return p.classList.add('up');
+    };
+
+    a.onblur = function () {
+        if (a.value.length != 0) {
+            return p.classList.add('up');
+        }
+        return p.classList.remove('up');
+    };
+};
+
+up(g('name'), g('plName'));
+up(g('email'), g('plEmail'));
+up(g('object'), g('plObject'));
+up(g('Massage'), g('plMassage'));
